9c00d09ea606099a395b552bc12b088803162e11
outwake <larissa.mendy@gmail.com>
2026-02-05 14:03:55 -0300

    :SPARKLES: ADD CONTA POUPANÇA

---
+++ src/model/ContaPoupança.ts
@@ -0,0 +1,29 @@
+import { Conta } from "./Conta";
+import { colors } from "../util/Colors";
+
+
+export class ContaPoupança extends Conta{
+
+    private _diaAniversario: number;
+
+
+	constructor(numero: number, agencia: number, titular: string, tipo: number, saldo: number , diaAniversario: number) {
+        super(numero, agencia, titular, tipo, saldo)
+		this._diaAniversario = diaAniversario;
+	}
+
+
+	public get diaAniversario(): number {
+		return this._diaAniversario;
+	}
+
+	public set diaAniversario(value: number) {
+		this._diaAniversario = value;
+	}
+
+    public visualizar(): void {
+        super.visualizar();
+        console.log(colors.fg.greenstrong,`Dia do Aniversário: ${this._diaAniversario} \n ` )
+    }
+    
+}
